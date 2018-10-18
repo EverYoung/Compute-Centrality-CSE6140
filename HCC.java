347ffbd3599330d0c9b75b2aefd9ff3e2d79845b
Xiao Yang <42979689+EverYoung@users.noreply.github.com>
2018-10-18 10:53:05 -0400

    Update HCC.java

--- HCC.java
+++ HCC.java
@@ -3,13 +3,10 @@ import java.util.Queue;
 
 /**
  * HCC.java
- * @author <your name here>
+ * Xiao Yang <your name here>
  * Georgia Institute of Technology, Fall 2018
  *
  * Heist-Closeness Centrality computation implementation
- * 
- * NOTE: You should change this file to add in your implementation.
- * Feel free to create as many local functions as you want.
  */
 
 public class HCC {
